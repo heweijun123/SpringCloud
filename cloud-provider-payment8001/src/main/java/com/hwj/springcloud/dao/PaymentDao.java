07f9364e8e587e53db2f096d9e11ee84e44835a6
heweijun <419544939@qq.com>
2021-08-28 14:21:48 +0800

    asd

---
+++ cloud-provider-payment8001/src/main/java/com/hwj/springcloud/dao/PaymentDao.java
@@ -0,0 +1,13 @@
+package com.hwj.springcloud.dao;
+
+import com.hwj.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PaymentDao {
+
+    public int create(Payment payment);
+
+    public Payment getPaymentById(@Param("id") Long id);
+}
---
+++ cloud-provider-payment8001/src/main/java/com/hwj/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,25 @@
+package com.hwj.springcloud.service.impl;
+
+import com.hwj.springcloud.dao.PaymentDao;
+import com.hwj.springcloud.entities.Payment;
+import com.hwj.springcloud.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+    @Autowired
+    private PaymentDao paymentDao;
+    @Override
+    public int create(Payment payment) {
+
+        return paymentDao.create(payment);
+    }
+
+    @Override
+    public Payment getPaymentById(Long id) {
+
+        return paymentDao.getPaymentById(id);
+    }
+}
